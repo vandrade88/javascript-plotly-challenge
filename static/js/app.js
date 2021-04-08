77bb591a8a6585bd341884fe4181486ebf669838
Valerie <vandrade07@yahoo.com>
2021-04-08 00:32:05 -0400

    updated index

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+// use d3 to read in samples.json
+
+
+// create horizontal bar chart with dropdown menu
+
+
+// create bubble chart that display each sample
+
+
+// display individual's demographic data
+
+
+// display key-value pairs from metadata JSON object
+
+
+// update all plots when dropdown option is selected
+
