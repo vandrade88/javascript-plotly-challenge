7869a35f95d396e6ebb48ef5d4ae76d33679ef18
Valerie <vandrade07@yahoo.com>
2021-04-08 02:21:04 -0400

    updated

--- static/js/app.js
+++ static/js/app.js
@@ -3,19 +3,37 @@
 
 // create horizontal bar chart with dropdown menu
 // id="bar"
+function buildBarChart(sampleNumber) {
+    d3.json("samples.json").then((data) => {
+        var sample_object = data.samples;
+        var sampleSelected = sample_object.filter(item => item.id === sampleNumber);
+        sampleSelected = sampleSelected[0];
+        var sample_values = sample_object.sample_values;
+        var otu_ids = sample_object.otu_ids;
+        var otu_labels = sample_object.otu_labels;
+
+}
 
 // create bubble chart that display each sample
 // id="bubble"
 
+
 // display individual's demographic data
-// id="sample-metadata"
 function buildMetaData(sampleNumber) {
     d3.json("samples.json").then((data) => {
         var metadata = data.metadata;
         var sampleSelected = metadata.filter(item => item.id === sampleNumber);
-
+        sampleSelected = sampleSelected[0];
+        // display key-value pairs from metadata JSON object
+        var metadata_object = d3.select("#sample-metadata");
+        metadata_object.html("");
+        Object.entries(sampleSelected).forEach(([key, value]) => {
+            metadata_object.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        })
     })
 }
+
+buildMetaData(943);
 
 // display key-value pairs from metadata JSON object
 
