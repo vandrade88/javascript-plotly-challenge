d7586f7e2834b0512813a4d79fb92e6c0638966c
Valerie <vandrade07@yahoo.com>
2021-04-08 00:41:24 -0400

    created outline for app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2,16 +2,21 @@
 
 
 // create horizontal bar chart with dropdown menu
-
+// id="bar"
 
 // create bubble chart that display each sample
-
+// id="bubble"
 
 // display individual's demographic data
-
+// id="sample-metadata"
 
 // display key-value pairs from metadata JSON object
 
 
 // update all plots when dropdown option is selected
+// onchange="optionChanged(this.value)
 
+// add options for dropdown
+// id="selDataset"
+
+// init function (default data)
